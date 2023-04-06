98096110b153b45a6d8694106fb0f56680b66a1b
ibnu naufal <ibnunaufal5758@gmail.com>
2023-04-06 13:58:37 +0700

    finsihing meal item

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_meals_app/dummy_data.dart';
+import 'package:flutter_meals_app/widgets/meal_item.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -10,21 +11,35 @@ class CategoryMealsScreen extends StatelessWidget {
 
   // CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
-
   @override
   Widget build(BuildContext context) {
-    final routeArgs = ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'] as String;
     final categoryId = routeArgs['id'] as String;
-    final categoryMeals = DUMMY_MEALS.where((element) {
-      return element.categories.contains(categoryId);
-    },).toList();
+    final categoryMeals = DUMMY_MEALS.where(
+      (element) {
+        return element.categories.contains(categoryId);
+      },
+    ).toList();
     return Scaffold(
-      appBar: AppBar(title: Text(categoryTitle),),
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
       body: Center(
-        child: ListView.builder(itemBuilder: (context, index) {
-          return Text(categoryMeals[index].title);
-        }, itemCount: categoryMeals.length,),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            final selected = categoryMeals[index];
+            return MealItem(
+                title: selected.title,
+                imageUrl: selected.imageUrl,
+                duration: selected.duration,
+                complexity: selected.complexity,
+                affordability: selected.affordability);
+            //MealItem(title: title, imageUrl: imageUrl, duration: duration, complexity: complexity, affordability: affordability);
+          },
+          itemCount: categoryMeals.length,
+        ),
       ),
     );
   }
