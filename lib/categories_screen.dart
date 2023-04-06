cec02fd0aeef4ca59269608e40272741a6d5b01b
ibnu naufal <ibnunaufal5758@gmail.com>
2023-04-06 11:54:10 +0700

    implement item with data

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import './dummy_data.dart';
+import './category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -9,12 +11,9 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView(
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200, 
-          childAspectRatio: 3 / 2
-          ),
-      children: [
-        
-      ],
+          maxCrossAxisExtent: 200, childAspectRatio: 3 / 2),
+      children:
+          DUMMY_CATEGORIES.map((e) => CategoryItem(e.title, e.color)).toList(),
     );
   }
 }
